81c78eddece6b292c6254eb82c87c903b02da25b
christopher-earl <cearl@umass.edu>
2026-05-25 21:30:05 +0900

    Basic working raster plot ticks

--- bindsnet/rendering/widgets.py
+++ bindsnet/rendering/widgets.py
@@ -13,11 +13,6 @@ class AbstractWidget:
     self.height = height    # Widget height
     self.x = x              # Bottom-left x coordinate
     self.y = y              # Bottom-right y coordinate
-    border_inset = min(width*0.05, height*0.05)   # padding from edges of widget to border
-    self.drawable_width = int(self.width - 2*border_inset)
-    self.drawable_height = int(self.height - 2*border_inset)
-    self.drawable_x = int(self.x + border_inset)
-    self.drawable_y = int(self.y + border_inset)
 
     ### Widget border rendering ###
     vertices = np.array([
@@ -80,8 +75,7 @@ class AbstractWidget:
     gl.glBindVertexArray(0)
 
   def render(self, time_step: int):
-    # Set size of area we are rendering into
-    gl.glViewport(self.drawable_x, self.drawable_y, self.drawable_width, self.drawable_height)
+    pass
 
 class RasterPlotWidget(AbstractWidget):
   # language=rst
@@ -100,7 +94,7 @@ class RasterPlotWidget(AbstractWidget):
       x:float,
       y:float,
       layer_name: int,
-      tick_spacing: int=20,
+      tick_spacing: int=200,
     ):
     super().__init__(width, height, x, y)
     self.layer_name = layer_name
@@ -110,8 +104,17 @@ class RasterPlotWidget(AbstractWidget):
     self.layer_size = None    # Assigned when App.add_widget() called
     self.tick_spacing = tick_spacing
     self.layer = None
+    border_inset = min(width*0.1, height*0.1)  # padding from edges of widget to border
+    self.drawable_width = int(self.width - 2*border_inset)
+    self.drawable_height = int(self.height - 2*border_inset)
+    self.drawable_x = int(self.x + border_inset)  # Leave room on right for tick/axis labels
+    self.drawable_y = int(self.y + border_inset)  # Leave room on bottom for tick/axis labels
+    self.x_tick_width = self.drawable_width
+    self.x_tick_height = int(height*0.05)
+    self.x_tick_x = self.drawable_x
+    self.x_tick_y = self.drawable_y - self.x_tick_height
 
-    ### Define shaders ###
+    ### Define raster shaders ###
     raster_texture_vertex_shader = """
         #version 330 core
 
@@ -188,6 +191,56 @@ class RasterPlotWidget(AbstractWidget):
     gl.glEnableVertexAttribArray(0)
     gl.glBindVertexArray(0)
 
+    ### Define tick shaders ###
+    tick_vertex_shader = """
+    #version 330 core
+
+    layout(location = 0) in vec2 pos;
+    void main()
+    {
+        gl_Position = vec4(pos, 0.0, 1.0);
+    }
+    """
+
+    tick_fragment_shader = """
+    #version 330 core
+
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    """
+    self.tick_program = compileProgram(
+      compileShader(tick_vertex_shader, gl.GL_VERTEX_SHADER),
+      compileShader(tick_fragment_shader, gl.GL_FRAGMENT_SHADER)
+    )
+
+    ### Define tick VAO/VBO ###
+    self.tick_vao = gl.glGenVertexArrays(1)
+    self.tick_vbo = gl.glGenBuffers(1)
+    gl.glBindVertexArray(self.tick_vao)
+    gl.glBindBuffer(
+      gl.GL_ARRAY_BUFFER,
+      self.tick_vbo
+    )
+    gl.glBufferData(
+      gl.GL_ARRAY_BUFFER,
+      1024 * 1024,    # TODO: Set to max number of possible ticks
+      None,
+      gl.GL_DYNAMIC_DRAW
+    )
+    gl.glVertexAttribPointer(
+      0,            # VAO slot
+      2,            # x,y
+      gl.GL_FLOAT,  # Data type
+      False,        # Normalized?
+      0,            # Stride
+      None          # Offset in buffer
+    )
+    gl.glEnableVertexAttribArray(0)
+    gl.glBindVertexArray(0)
+
   def prime(self, network: GUINetwork):
     self.layer_size = network.layers[self.layer_name].n
     self.spikes_vbo = network.opengl_vbos['layers'][self.layer_name]['s']
@@ -221,10 +274,74 @@ class RasterPlotWidget(AbstractWidget):
 
     self.layer = network.layers[self.layer_name]
 
-  def render_ticks(self):
-    ...
+  def render_ticks(self, time_step: int) -> None:
+    # Set size of area we are rendering into
+    gl.glViewport(self.x_tick_x, self.x_tick_y, self.x_tick_width, self.x_tick_height)
 
-  def render_spikes(self, time_step: int):
+    gl.glEnable(gl.GL_SCISSOR_TEST)
+
+    gl.glScissor(
+      self.x_tick_x,
+      self.x_tick_y,
+      self.x_tick_width,
+      self.x_tick_height
+    )
+
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+    gl.glDisable(gl.GL_SCISSOR_TEST)
+
+    ### Calculate tick labels and position vertices ###
+    t_s = time_step - self.max_time_steps    # Oldest time step currently visible in raster plot
+    # Labels
+    label_range = (
+      max(t_s + (self.tick_spacing - (t_s % self.tick_spacing)), 0),
+      time_step - (time_step % self.tick_spacing)
+    )
+    labels = np.arange(
+      label_range[0],
+      label_range[1] + 1, self.tick_spacing
+    )
+    # Tick positions
+    tick_x_pos = (
+        (labels - t_s)
+        / self.max_time_steps
+    )
+    tick_x_pos = (
+      tick_x_pos * 2.0
+    ) - 1.0
+    # tick_x_pos = (((tick_x_pos - t_s) / self.max_time_steps) * 2) - 1  # Normalize to [-1,1] for shader
+    # Vertices
+    y_top = 1.0
+    y_bot = 0.3
+    vertices = np.array(
+    [
+        [x, y_top,
+         x, y_bot]
+        for x in tick_x_pos
+    ], dtype=np.float32
+    ).flatten()
+
+    ### Render ###
+    gl.glBindBuffer(
+      gl.GL_ARRAY_BUFFER,
+      self.tick_vbo
+    )
+    gl.glBufferSubData(
+      gl.GL_ARRAY_BUFFER,
+      0,
+      vertices.nbytes,
+      vertices
+    )
+    gl.glUseProgram(self.tick_program)
+    gl.glBindVertexArray(self.tick_vao)
+    gl.glDrawArrays(gl.GL_LINES, 0, len(vertices)//2)
+    gl.glBindVertexArray(0)
+
+  def render_spikes(self, time_step: int) -> None:
+    # Set size of area we are rendering into
+    gl.glViewport(self.drawable_x, self.drawable_y, self.drawable_width, self.drawable_height)
+
     wrapped_t = time_step % self.max_time_steps
 
     ### Migrate spike data to GPU ###
@@ -238,8 +355,8 @@ class RasterPlotWidget(AbstractWidget):
       gl.GL_TEXTURE_2D,
       0,
       wrapped_t,  # x offset
-      0,  # y offset
-      1,  # width
+      0,          # y offset
+      1,          # width
       self.layer_size,  # height
       gl.GL_RED,
       gl.GL_UNSIGNED_BYTE,
@@ -247,26 +364,24 @@ class RasterPlotWidget(AbstractWidget):
     )
 
     ### Plot ###
+    # Pass write head and length of history to shader
     gl.glUseProgram(self.raster_plot_program)
     gl.glUniform1f(
       gl.glGetUniformLocation(self.raster_plot_program, "write_head"),
       wrapped_t
     )
-
-    gl.glUniform1f(
+    gl.glUniform1f(   # TODO: Can this be manually added into shader string definition?
       gl.glGetUniformLocation(self.raster_plot_program, "history_width"),
       self.max_time_steps
     )
 
+    # Draw texture (spikes)
     gl.glActiveTexture(gl.GL_TEXTURE0)
-
     gl.glBindTexture(
       gl.GL_TEXTURE_2D,
       self.raster_texture
     )
-
     gl.glBindVertexArray(self.quad_vao)
-
     gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
 
     glfw.swap_buffers(self.window)
@@ -275,5 +390,5 @@ class RasterPlotWidget(AbstractWidget):
   def render(self, time_step: int):
     super().render(time_step)
     # self.render_background()
-    # self.render_ticks()
+    self.render_ticks(time_step)
     self.render_spikes(time_step)
